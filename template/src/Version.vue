34eb46ed89c643f67d1ebce3060ba3bb4d3b5af0
Bernt Anker <bernt.anker@me.com>
2017-10-28 23:25:57 +0200

    initial commit

---
+++ template/src/Version.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="version">
+    You are using Vue {{version}}
+  </div>
+</template>
+<script type="text/javascript">
+import Vue from 'vue'
+export default {
+  name: 'vue-version',
+  computed: {
+    version () {
+      return Vue.version
+    }
+  }
+}
+</script>
+<style>
+.version {
+  font-weight: bold;
+  font-family: tahoma;
+}
+</style>
